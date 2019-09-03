b0fb93aac2f72c8069737cf32a3ab7916e0412c2
tknieza <tadasknieza@gmail.com>
2019-09-03 13:22:05 +0300

    add commnets

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,7 +12,6 @@ const App = () => {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
     const url = `https://twitter.com/search?q=${querry}&src=typd&f=image`; // site doesn’t send Access-Control-*
-    let tweets = [];
 
     fetch(proxyurl + url, {
       method: "GET",
@@ -22,32 +21,33 @@ const App = () => {
     })
       .then(response => response.text())
       .then(contents => {
-        // Loading in the response HTML from url
+        // Loading in response HTML from url
         const $ = cheerio.load(contents);
 
+        // Getting all tweets in the page
         const tweetElements = $(".tweet");
 
-        tweetElements.toArray().forEach((item, index) => {
-          tweets.push({
+        // Serializing tweetElements into something readable
+        const serializedTweets = tweetElements.toArray().map(item => {
+          return {
             name: item.attribs["data-name"],
             screenName: item.attribs["data-screen-name"],
             userId: item.attribs["data-user-id"],
             permaLink: item.attribs["data-permalink-path"],
+            id: item.attribs["data-tweet-id"],
             images: $("img", item)
               .toArray()
               .map(item => {
                 return item.attribs.src;
               })
-          });
+          };
         });
+
+        setTweets(serializedTweets);
       })
       .catch(() =>
         console.log(`Can’t access ${url} response. Blocked by browser?`)
-      )
-      .finally(() => {
-        console.log(tweets, "Tweets");
-        setTweets(tweets);
-      });
+      );
   };
 
   const handleSubmit = event => {
